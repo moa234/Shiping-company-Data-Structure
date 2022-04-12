dbbce12ce62c55ad437fd185d2113240a419cfaa
moa234 <moaaztarek@hotmail.com>
2022-04-12 17:16:05 +0200

    basics cargos

--- Shiping company Data Structure/Cargo.h
+++ Shiping company Data Structure/Cargo.h
@@ -1,5 +1,27 @@
 #pragma once
 class Cargo
 {
+	struct time
+	{
+		int Day;
+		int Hour;
+		//add day when hour > 24
+	};
+	struct loading
+	{
+		int load;
+		int unload;
+	};
+protected:
+	loading l_u; //load and unload time in hours
+	time prept; //preparation time in (day:hour)
+	int deldis; //delivery distance in km
+	int cost; //cost of delivering truck
+
+
+public:
+	bool virtual load() = 0;
+	bool virtual unload() = 0;
+	//bool virtual assign() = 0;
 };
 
