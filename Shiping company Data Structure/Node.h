13296d2030666c9afca4521306d05a4da0604fbd
Salah Abotaleb <salah.m.abotaleb@gmail.com>
2022-04-12 19:32:00 +0200

    node

--- Shiping company Data Structure/Node.h
+++ Shiping company Data Structure/Node.h
@@ -8,7 +8,9 @@ public:
 	Node();
 	Node(const T& item);
 	Node(const T& item,Node<T>* nxt);
-
+	Node<T>* getnext() const;
+	T getitem() const;
+	void setnext(Node<T>* ptr);
 };
 
 template<typename T>
@@ -28,3 +30,21 @@ template<typename T>
 	 data = item;
 	 next = nxt;
 }
+
+ template<typename T>
+  Node<T>* Node<T>::getnext() const
+ {
+	 return next;
+ }
+
+  template<typename T>
+  T Node<T>::getitem() const
+  {
+	  return data;
+  }
+
+  template<typename T>
+  void Node<T>::setnext(Node<T>* ptr)
+  {
+	  next = ptr;
+  }
