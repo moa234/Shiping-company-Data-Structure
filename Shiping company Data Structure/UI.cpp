4d302394a1b1d2e5123f6969f2e720f773f78568
moa234 <moaaztarek@hotmail.com>
2022-04-16 21:02:28 +0200

    mode

--- Shiping company Data Structure/UI.cpp
+++ Shiping company Data Structure/UI.cpp
@@ -9,10 +9,20 @@ int UI::readnumber()
 	return x;
 }
 
+void UI::displaytext(string c)
+{
+	cout << c << endl;
+}
+
 void UI::readmode()
 {
 	cout << "Choose mode of opeartion" << endl;
 	cout << "For interactive mode enter 1" << endl;
 	cout << "For interactive mode enter 2" << endl;
 	cout << "For interactive mode enter 3" << endl;
+	displaytext("Choose mode of opeartion");
+	displaytext("For interactive mode enter 1");
+	displaytext("For step-by-step mode enter 2");
+	displaytext("For silent mode enter 3");
+	mode = (readnumber() == 0)? interactive: (readnumber() == 1) ?stepBstep:silent ;
 }
