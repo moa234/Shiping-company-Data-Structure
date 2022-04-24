f7aa15705e7819adc72040678e7aee7c55cdd6d3
Salah Abotaleb <salah.m.abotaleb@gmail.com>
2022-04-24 22:23:13 +0200

    tt

--- Shiping company Data Structure/Truck.cpp
+++ Shiping company Data Structure/Truck.cpp
@@ -54,7 +54,7 @@ void Truck::setDI(int DeliveryT)
 
 void Truck::IncementJ()
 {
-	Currjourney = 0;
+	Currjourney++;
 }
 
 std::ostream& operator<<(std::ostream& f, const Truck* C)
@@ -62,3 +62,4 @@ std::ostream& operator<<(std::ostream& f, const Truck* C)
 	f << C->getid();
 	return f;
 }
+
